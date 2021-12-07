4f67de5e52a9bd505a15e9e0e19f75f0dd4f8a17
Shivam Tewari <www.shivamtewari@gmail.com>
2021-12-07 19:30:21 +0530

    fix: quiz not progressing

--- app/src/main/java/com/example/quizapp/SampleQuiz.java
+++ app/src/main/java/com/example/quizapp/SampleQuiz.java
@@ -26,10 +26,10 @@ public class SampleQuiz extends AppCompatActivity {
     private Toolbar tb;
     private TextView timer_tv;
     private ProgressBar timer;
-    private int progress = 100;
+    private int progress;
 
     private CountDownTimer timer_ref;
-    private long timeLeftInMilliseconds = 60_000;
+    private long timeLeftInMilliseconds;
     private boolean isTimerRunning;
 
     Typeface primary_tf;
@@ -59,28 +59,21 @@ public class SampleQuiz extends AppCompatActivity {
         String quizName = quiz_selected.getStringExtra("quiz_name");
 
         timer_tv = (TextView) findViewById(R.id.text_view_progress);
+        timeLeftInMilliseconds = 60_000;
         timer_tv.setText(String.valueOf("1:00"));
 
         timer = (ProgressBar) findViewById(R.id.progress_bar);
-        timer.setProgress(100);
-
-        next_btn = (Button) findViewById(R.id.button2);
+        int timeLeftInitially = (int)timeLeftInMilliseconds/1000;
+        timer.setMax(timeLeftInitially);
+        progress = timeLeftInitially;
+        timer.setProgress(progress);
 
         ques = (TextView) findViewById(R.id.question);
         opt_a = (Button) findViewById(R.id.option_a);
         opt_b = (Button) findViewById(R.id.option_b);
         opt_c = (Button) findViewById(R.id.option_c);
         opt_d = (Button) findViewById(R.id.option_d);
-
-        ques.setTypeface(primary_tf);
-        opt_a.setTypeface(primary_tf);
-        opt_b.setTypeface(primary_tf);
-        opt_c.setTypeface(primary_tf);
-        opt_d.setTypeface(primary_tf);
-        next_btn.setTypeface(primary_tf);
-        timer_tv.setTypeface(primary_tf);
-
-
+        next_btn = (Button) findViewById(R.id.button2);
 
         switch(quizName) {
             case "Intro Quiz":  introQuizDBHandler = new IntroQuizDBHandler(SampleQuiz.this);
@@ -91,10 +84,14 @@ public class SampleQuiz extends AppCompatActivity {
                                         break;
         }
 
-        QuizModal questionData = questionsModalArrayList.get(0);
+        final int[] questionIndex = {0};
+        QuizModal questionData = questionsModalArrayList.get(questionIndex[0]);
 
         String question = questionData.getQuestionName();
+        final String[] correctAnswer = {questionData.getCorrectAnswer()};
         ArrayList<String> options = new ArrayList<>();
+        final int[] score = {0};
+        int maxScore = questionsModalArrayList.size() * 1;
 
         String optionStr = questionData.getOptions();
         int startIndex = 0;
@@ -114,140 +111,119 @@ public class SampleQuiz extends AppCompatActivity {
         opt_c.setText(options.get(2));
         opt_d.setText(options.get(3));
 
-
         opt_a.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 opt_a.setBackgroundColor(getResources().getColor(R.color.peri_winkle));
                 opt_b.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
                 opt_c.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
                 opt_d.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
-
             }
         });
         opt_b.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 opt_b.setBackgroundColor(getResources().getColor(R.color.peri_winkle));
                 opt_a.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
                 opt_c.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
                 opt_d.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
-
             }
         });
         opt_c.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 opt_c.setBackgroundColor(getResources().getColor(R.color.peri_winkle));
                 opt_a.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
                 opt_b.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
                 opt_d.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
-
             }
         });
         opt_d.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 opt_d.setBackgroundColor(getResources().getColor(R.color.peri_winkle));
                 opt_a.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
                 opt_b.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
                 opt_c.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
-
             }
         });
-
 
         next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 stopTimer();
 
-                for(int questionIndex = 0; questionIndex < questionsModalArrayList.size(); questionIndex ++) {
+                int opt_a_color_id = ((ColorDrawable)opt_a.getBackground()).getColor();
+                int opt_b_color_id = ((ColorDrawable)opt_b.getBackground()).getColor();
+                int opt_c_color_id = ((ColorDrawable)opt_c.getBackground()).getColor();
+                int opt_d_color_id = ((ColorDrawable)opt_d.getBackground()).getColor();
 
-                    if(questionIndex == 0) {
-                        QuizModal questionData = questionsModalArrayList.get(questionIndex);
-                        String correctAnswer = questionData.getCorrectAnswer();
 
-                        int opt_a_color_id = ((ColorDrawable)opt_a.getBackground()).getColor();
-                        int opt_b_color_id = ((ColorDrawable)opt_b.getBackground()).getColor();
-                        int opt_c_color_id = ((ColorDrawable)opt_c.getBackground()).getColor();
-                        int opt_d_color_id = ((ColorDrawable)opt_d.getBackground()).getColor();
+                String selectedOption = "";
 
-                        String selectedOption;
+                if(opt_a_color_id == getResources().getColor(R.color.peri_winkle)) {
+                    selectedOption = opt_a.getText().toString();
+                } else if(opt_b_color_id == getResources().getColor(R.color.peri_winkle)) {
+                    selectedOption = opt_b.getText().toString();
+                } else if(opt_c_color_id == getResources().getColor(R.color.peri_winkle)) {
+                    selectedOption = opt_c.getText().toString();
+                } else if(opt_d_color_id == getResources().getColor(R.color.peri_winkle)) {
+                    selectedOption = opt_d.getText().toString();
+                }
 
-                        if(opt_a_color_id == getResources().getColor(R.color.peri_winkle)) {
-                            selectedOption = opt_a.getText().toString();
-                        } else if(opt_b_color_id == getResources().getColor(R.color.peri_winkle)) {
-                            selectedOption = opt_b.getText().toString();
-                        } else if(opt_c_color_id == getResources().getColor(R.color.peri_winkle)) {
-                            selectedOption = opt_c.getText().toString();
-                        } else {
-                            selectedOption = opt_d.getText().toString();
+                if(selectedOption.equals(correctAnswer[0])) {
+                    displayToast("☺️ Correct");
+                    score[0] += 1;
+                }
+                else if(selectedOption.equals("")) {
+                    displayToast("Correct Answer was " + correctAnswer[0]);
+                }
+                else displayToast("\uD83D\uDE22 Wrong\nCorrect Answer was " + correctAnswer[0]);
+
+                if(questionIndex[0] +1 < questionsModalArrayList.size()) {
+                    questionIndex[0] +=1;
+
+                    QuizModal questionData = questionsModalArrayList.get(questionIndex[0]);
+
+                    String question = questionData.getQuestionName();
+                    correctAnswer[0] = questionData.getCorrectAnswer();
+                    ArrayList<String> options = new ArrayList<>();
+
+                    String optionStr = questionData.getOptions();
+                    int startIndex = 0;
+                    int endIndex = 0;
+                    for(int index = 0; index < optionStr.length(); index++) {
+                        char currChar = optionStr.charAt(index);
+                        if(currChar == ';') {
+                            endIndex = index;
+                            options.add(optionStr.substring(startIndex, endIndex));
+                            startIndex = endIndex + 1;
                         }
-
-                        if(selectedOption.equals(correctAnswer)) {
-                            displayToast("☺️ Correct");
-                        }
-                        else displayToast("\uD83D\uDE22 Wrong");
                     }
 
-                    else {
+                    opt_a.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
+                    opt_b.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
+                    opt_c.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
+                    opt_d.setBackgroundColor(getResources().getColor(R.color.exodus_fruit));
 
-                        QuizModal questionData = questionsModalArrayList.get(questionIndex);
+                    ques.setText(question);
+                    opt_a.setText(options.get(0));
+                    opt_b.setText(options.get(1));
+                    opt_c.setText(options.get(2));
+                    opt_d.setText(options.get(3));
 
-                        String question = questionData.getQuestionName();
-                        String correctAnswer = questionData.getCorrectAnswer();
-                        ArrayList<String> options = new ArrayList<>();
+                    timerManager();
+                }
+                else {
+                    setContentView(R.layout.quiz_results);
+                    tb = (Toolbar) findViewById(R.id.quiz_result_toolbar);
+                    setSupportActionBar(tb);
 
-                        String optionStr = questionData.getOptions();
-                        int startIndex = 0;
-                        int endIndex = 0;
-                        for(int index = 0; index < optionStr.length(); index++) {
-                            char currChar = optionStr.charAt(index);
-                            if(currChar == ';') {
-                                endIndex = index;
-                                options.add(optionStr.substring(startIndex, endIndex));
-                                startIndex = endIndex + 1;
-                            }
-                        }
-
-                        ques.setText(question);
-                        opt_a.setText(options.get(0));
-                        opt_b.setText(options.get(1));
-                        opt_c.setText(options.get(2));
-                        opt_d.setText(options.get(3));
-
-                        int opt_a_color_id = ((ColorDrawable)opt_a.getBackground()).getColor();
-                        int opt_b_color_id = ((ColorDrawable)opt_b.getBackground()).getColor();
-                        int opt_c_color_id = ((ColorDrawable)opt_c.getBackground()).getColor();
-                        int opt_d_color_id = ((ColorDrawable)opt_d.getBackground()).getColor();
-
-                        String selectedOption;
-
-                        if(opt_a_color_id == getResources().getColor(R.color.peri_winkle)) {
-                            selectedOption = opt_a.getText().toString();
-                        } else if(opt_b_color_id == getResources().getColor(R.color.peri_winkle)) {
-                            selectedOption = opt_b.getText().toString();
-                        } else if(opt_c_color_id == getResources().getColor(R.color.peri_winkle)) {
-                            selectedOption = opt_c.getText().toString();
-                        } else {
-                            selectedOption = opt_d.getText().toString();
-                        }
-
-                        if(selectedOption.equals(correctAnswer)) {
-                            displayToast("☺️ Correct");
-                        }
-                        else displayToast("\uD83D\uDE22 Wrong");
-
-                    }
+                    TextView scoreText = (TextView) findViewById(R.id.score_text_view);
+                    scoreText.setText("You scored "+score[0]+"/"+maxScore+" points !");
                 }
             }
         });
-
         timerManager();
     }
 
@@ -259,6 +235,7 @@ public class SampleQuiz extends AppCompatActivity {
     }
 
     private void startTimer() {
+        timeLeftInMilliseconds = 60_000;
         timer_ref = new CountDownTimer(timeLeftInMilliseconds, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
@@ -268,7 +245,7 @@ public class SampleQuiz extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                displayToast("Your response submitted successfully");
+                next_btn.performClick();
             }
         }.start();
         isTimerRunning = true;
@@ -283,7 +260,7 @@ public class SampleQuiz extends AppCompatActivity {
         int minutes = (int) timeLeftInMilliseconds / 60_000;
         int seconds = (int) (timeLeftInMilliseconds % 60_000) / 1000;
 
-        progress -= (int) (100/60) ;
+        progress = (int)timeLeftInMilliseconds/1000 ;
         timer.setProgress(progress);
         if(seconds<10) {
             timer_tv.setText(minutes+":0"+seconds);
@@ -301,7 +278,8 @@ public class SampleQuiz extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()) {
-            case R.id.back_btn: this.finish();
+            case R.id.back_btn: stopTimer();
+                                this.finish();
                                 return true;
             default:
                 return super.onOptionsItemSelected(item);
